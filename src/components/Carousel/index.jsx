0f83f76a2474656b5ee5bed65d78b2abad9e9050
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-03-10 21:55:09 -0300

    Created Home page

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './carouselStyles.scss'
+
+const Carousel = () => {
+  return (
+    <div className='carousel'>Carousel</div>
+  )
+}
+
+export default Carousel
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,12 +1,32 @@
-import { Navbar } from "../../components/Navbar"
+import Carousel from "../../components/Carousel";
+import { Navbar } from "../../components/Navbar";
+import nwLogo from "../../assets/neloworksLogo.png";
+import "./homeStyles.scss";
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <h1>Home</h1>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Navbar />
+            <Carousel />
+            <div className="content_nw">
+                <img
+                    src={nwLogo}
+                    alt="NeloWorks Logo alfombras personalizadas"
+                />
+                <p className="slogan">
+                    Diseñamos tus ideas, hacemos alfombras, creamos comodidad.
+                    Ve nuestros diseños en instagram{" "}
+                    <a
+                        href="https://www.instagram.com/neloworks/"
+                        target="__blank"
+                        rel="noopener noreferrer"
+                    >
+                        @neloworks
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
