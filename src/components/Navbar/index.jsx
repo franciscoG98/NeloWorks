fa32f939665817db4d84286313bc085d8a5b5e37
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-03-10 21:24:07 -0300

    Created navbar responsive component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbarStyles.scss";
+import logo from "../../assets/logoNWlineas.png";
 
 export const Navbar = () => {
-  return (
-    <nav>
-        <ul>
-            <Link to='/'>Inicio</Link>
-            <Link to='/informacion'>Informacion</Link>
-            <Link to='/productos'>Productos</Link>
-            <Link to='/galeria'>Galeria</Link>
-            <Link to='/contacto'>Contacto</Link>
-        </ul>
-    </nav>
-  )
-}
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <nav className="navbar">
+            <div className="nav_logo">
+                <Link to="/">
+                    <img src={logo} alt="Logo Neloworks" />
+                </Link>
+            </div>
+
+            <div className={`nav_list ${isOpen && "open"}`}>
+                <Link to="/" className="nav_item">
+                    Inicio
+                </Link>
+                <Link to="/informacion" className="nav_item">
+                    Informacion
+                </Link>
+                <Link to="/productos" className="nav_item">
+                    Productos
+                </Link>
+                <Link to="/galeria" className="nav_item">
+                    Galeria
+                </Link>
+                <Link to="/contacto" className="nav_item">
+                    Contacto
+                </Link>
+            </div>
+
+            <div className={`nav_toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+        </nav>
+    );
+};
